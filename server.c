0d56abe0473a1761cae6d4b0d2b39b29bc84d792
Mahmadou <146755529+Mahmadou@users.noreply.github.com>
2024-01-19 17:05:55 +0100

    Update server.c

--- server.c
+++ server.c
@@ -1,45 +1,35 @@
-#include <signal.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <time.h>
+#include <signal.h>
+#include <errno.h>
 
-#define MAX_MESSAGE_LENGTH 1024
+volatile sig_atomic_t received_signal = 0;
 
-char message[MAX_MESSAGE_LENGTH];
-int current_char = 0;
-
-void handle_signal(int signal) {
-    if (signal == SIGUSR1 || signal == SIGUSR2) {
-        message[current_char] = (message[current_char] << 1) | (signal == SIGUSR2);
-        if (++current_char >= MAX_MESSAGE_LENGTH || message[current_char - 1] == '\0') {
-            char timestamp[64];
-            time_t now = time(NULL);
-            strftime(timestamp, sizeof(timestamp), "[%H:%M:%S - %d/%m/%Y]", localtime(&now));
-
-            printf("%s : %s\n", timestamp, message);
-
-            FILE *log_file = fopen("messages.logs", "a");
-            if (log_file != NULL) {
-                fprintf(log_file, "%s : %s\n", timestamp, message);
-                fclose(log_file);
-            }
-
-            memset(message, 0, MAX_MESSAGE_LENGTH);
-            current_char = 0;
-        }
+// Gestionnaire de signal pour le serveur
+void signal_handler(int signum) {
+    if (signum == SIGUSR1 || signum == SIGUSR2) {
+        received_signal = signum;
     }
 }
 
+// Fonction principale du serveur
 int main() {
-    printf("Server PID: %d\n", getpid());
+    signal(SIGUSR1, signal_handler);
+    signal(SIGUSR2, signal_handler);
 
-    struct sigaction sa = { .sa_handler = &handle_signal };
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);
+    printf("Serveur en attente de signaux...\n");
 
     while (1) {
         pause();
+
+        if (received_signal == SIGUSR1) {
+            printf("0");
+        } else if (received_signal == SIGUSR2) {
+            printf("1");
+        }
+        
+        received_signal = 0;
     }
 
     return 0;
