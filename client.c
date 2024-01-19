278e5fcb3bbe7d86f46a945e0f7c0473d0db1efb
Mahmadou <146755529+Mahmadou@users.noreply.github.com>
2024-01-19 17:05:08 +0100

    Update client.c

--- client.c
+++ client.c
@@ -1,33 +1,41 @@
-#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
 
-void send_char_as_signals(char c, pid_t server_pid) {
+#define DELAY 500
+
+// Fonction pour envoyer un bit sous forme de signal.
+void send_bit_signal(pid_t server_pid, int bit_signal) {
+    if (kill(server_pid, bit_signal) == -1) {
+        perror("Erreur lors de l'envoi du signal");
+        exit(EXIT_FAILURE);
+    }
+    usleep(DELAY);
+}
+
+// Fonction pour envoyer un caractère au serveur en séquence de signaux.
+void send_char_as_signals(char character, pid_t server_pid) {
     for (int i = 7; i >= 0; --i) {
-        kill(server_pid, (c >> i) & 1 ? SIGUSR2 : SIGUSR1);
-        usleep(500); // 0.5ms
+        int bit = (character >> i) & 1;
+        send_bit_signal(server_pid, bit ? SIGUSR2 : SIGUSR1);
     }
 }
 
+// Fonction principale du client
 int main(int argc, char *argv[]) {
-    // Vérifier le nombre d'arguments
-    if (argc != 3) {
-        printf("Usage: %s <Server PID> <Message>\n", argv[0]);
-        return 1;
+    if (argc != 3 || atoi(argv[1]) <= 0) {
+        fprintf(stderr, "Usage: %s <Server PID> <Message>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    // Récupérer le PID du serveur et le message à envoyer
     pid_t server_pid = atoi(argv[1]);
-    char *message = argv[2];
 
-    // Envoyer chaque caractère du message
-    for (int i = 0; i < strlen(message); ++i) {
-        send_char_as_signals(message[i], server_pid);
+    for (int i = 0; i < strlen(argv[2]); ++i) {
+        send_char_as_signals(argv[2][i], server_pid);
     }
-
-    // Envoyer le caractère nul à la fin du message
-    send_char_as_signals('\0', server_pid);
 
     return 0;
 }
